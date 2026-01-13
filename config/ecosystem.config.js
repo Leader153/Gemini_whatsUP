f9a09b095bb249c6e52e9672d94685527eb6f450
Leader153 <danielflider78@gmail.com>
2026-01-13 23:58:12 +0200

    feat: Finalize project cleanup and configuration

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   apps: [{
-    name: "leader-tunnel",
-    script: "cloudflared",
-    args: "tunnel --config config/config.yml run",
-    interpreter: "none",
-    cwd: "./",
-    exec_mode: "fork"
-  }, {
     name: "gemini-bot",
-    script: "handlers/answer_phone.js",
+    script: "index.js",
     watch: true,
     cwd: "./",
     ignore_watch: ["node_modules", "orders", "data", "logs", ".git"],
